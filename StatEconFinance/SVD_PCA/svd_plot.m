5c701455e64f813701bb4208e6e7454c8da061ac
Matthew Cocci <Matthew.Cocci@ny.frb.org>
2014-04-05 22:20:29 -0400

    Commit from ran

--- StatEconFinance/SVD_PCA/svd_plot.m
+++ StatEconFinance/SVD_PCA/svd_plot.m
@@ -17,7 +17,7 @@ hold on
 plot(YY, XX, 'color', [0.5, 0.5, 0.5])
 plot(ax1,ax2,'k', 'LineWidth', 2)
 plot(ax2,ax1,'k', 'LineWidth', 2)
-quiver(x(1),x(2), 'LineWidth', 3)
+quiver(0, 0, x(1), x(2), 0, 'LineWidth', 2)
 hold off
 x_lim = xlim;
 y_lim = ylim;
@@ -25,6 +25,7 @@ y_lim = ylim;
 
 %% Diagonal Matrix Case
 
+x = A_diag*x;
 figure()
 grid_new1 = A_diag*([XX(:), YY(:)]');
 grid_new2 = A_diag*([YY(:), XX(:)]');
@@ -35,9 +36,13 @@ hold on
 plot(reshape(grid_new2(1,:), size(XX)), ...
     reshape(grid_new2(2,:), size(YY)), ...
     'color', [0.5, 0.5, 0.5])
+quiver(0, 0, x(1), x(2), 0, 'LineWidth', 2)
 plot(ax1,ax2,'k', 'LineWidth', 2)
 plot(ax2,ax1,'k', 'LineWidth', 2)
 hold off
 axis([x_lim, y_lim])
 
+
+%% Symmetric Matrix Case
+A_symm = [2 1; 1 2];
 
