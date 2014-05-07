64cb742225cf4542c7bcc874bde2c451f1741c89
Matthew Cocci <matthew.cocci@gmail.com>
2014-05-07 02:12:26 -0400

    Figures/DevilsStaircase.R

--- Mathematics/Analysis/Figures/DevilsStaircase.R
+++ Mathematics/Analysis/Figures/DevilsStaircase.R
@@ -5,34 +5,66 @@
 #############################################
 
 
-# Function to remove the middle third, return the 
-
 # Steps
-#   1. Take a segment and a max height 
-#   2. Remove the middle third
-#   3. Plot that middle third at half the max height
-#   4. Redefine segment and max height; return to 1
+#   1. Take interval matrix, step over rows and put middle 
+#       third in matrices. Put remaining in new matrix.
+#   2. Figure out number of segment to plot at this stage.
+#   3. Space them and plot them accordingly
+#   4. Output is matrix containing remaining segments as rows, 
+#       endpoints as columns.
+#   5. Return to step 1
 
-n.iter <- 100
-max.height <- 1
-segment <- c(0,1)
 
-i <- 1
-while (i < n.iter) {
-    seg.len <- segment[2] - segment[1]
-    mid.third <- c(segment[1]+seg.len/3, 
-                   segment[2]-(seg.len)/3)
-    max.height <- max.height / 2
-    if (i==1) {
-        plot(mid.third, c(max.height, max.height), type="l", 
-             xlim=c(0,1), ylim=c(0,1))
-    } else {
-        lines(mid.third, c(max.height, max.height))
-        lines(1-mid.third, c(3*max.height, 3*max.height))
-    }
-    segment = c(0, mid.third[1])
-    i <- i+1
-    readline()
+# Function to loop over matrix where rows are remaining segments.
+# Output:
+#   1. Plot Matrix
+#   2. Remaining matrix
+cantor.step <- function(in.matrix){
+
+    # Number of new segments; initialize out matrix
+    n.seg <- 2*nrow(in.matrix)
+    out.matrix <- matrix(0,n.seg,2)
+
+    # Length of new segments
+    seg.length <- 1/(3^nrow(in.matrix))
+
+    # Indicators for new segments close to 0 or 1
+    near0 <- as.logical(matrix(rbind(rep(1,n.seg/2),rep(0,n.seg/2)),n.seg,1))
+    near1 <- !near0
+
+    # Assign values to matrix   
+    out.matrix[near0,] <- in.matrix
+    out.matrix[near1,] <- in.matrix
+    plot.matrix <- in.matrix
+
+    # Add and subtract lengths
+    out.matrix[near0,2] <- out.matrix[near0,1] + seg.length
+    out.matrix[near1,1] <- out.matrix[near1,2] - seg.length
+    plot.matrix[,1] <- plot.matrix[,1] + seg.length
+    plot.matrix[,2] <- plot.matrix[,2] - seg.length
+
+    list(plot.matrix,out.matrix)
 }
 
 
+# Loop through and plot
+n.removals <- 3
+in.matrix <- matrix(c(0,1),1,2)
+plot(1,type="n",xlim=c(0,1),ylim=c(0,1),xlab="",ylab="")
+for(i in 1:n.removals){
+
+    output <- cantor.step(in.matrix) 
+    plot.matrix <- output[[1]]
+    in.matrix <- output[[2]]
+    
+    plot.count <- nrow(plot.matrix)
+    ylocs <- seq(1,2^plot.count,((2^plot.count)-1))/(2^plot.count)
+
+    for(i in 1:plot.count){
+        lines(plot.matrix[i,],rep(ylocs[i],2))
+    }
+}
+
+# 1/2 
+# 1/4, 3/4
+# 1/8, 3/8, 5/8, 7/8
