39bce1763d6869fb250f2d53e903052d04be09cb
Matthew Cocci <Matthew.Cocci@ny.frb.org>
2015-05-11 10:07:58 -0400

    Premerge commit

--- StatEconFinance/AppliedStochasticAnalysis/HW12/check.py
+++ StatEconFinance/AppliedStochasticAnalysis/HW12/check.py
@@ -1,6 +1,6 @@
 import sympy as sp
 
-x, y, s2, a, gam, mu, C = sp.symbols('x y s2 a gam mu C')
+x, y, s2, a, gam, mu, C, c, d = sp.symbols('x y s2 a gam mu C c d')
 
 # Early
 rho  = sp.exp(-(1/(s2))*((x-y)**2))
@@ -11,5 +11,11 @@ drho = rho.diff(x)
 rhs = -(gam/mu)*a + 1/a
 rho = sp.exp(sp.integrate(rhs, a)).simplify()
 rhonorm = (gam/mu)*a*sp.exp(-(gam/(2*mu))*(a**2))
+
+# Question 5b
+b = (1/4.)*(a+d)**2
+
+target = b.diff(a).diff(a) + (-gam*a + mu/a)*b.diff(a)
+
 
 #execfile('check.py')
